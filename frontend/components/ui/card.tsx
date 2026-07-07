138f0b4807b928775ca2692c04ef4b30156694cc
dzekojohn4 <232421169+dzekojohn4@users.noreply.github.com>
2026-07-07 09:07:26 +0000

    docs(frontend/ui): add JSDoc to Card components

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -2,8 +2,13 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
+/** Props for the {@link Card} root container. */
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+/**
+ * Themed card container with rounded corners and a subtle shadow.
+ * Forwarded refs attach to the underlying `<div>` element.
+ */
 const Card = React.forwardRef<HTMLDivElement, CardProps>(
   ({ className, ...props }, ref) => (
     <div
@@ -18,8 +23,12 @@ const Card = React.forwardRef<HTMLDivElement, CardProps>(
 );
 Card.displayName = "Card";
 
+/** Props for the {@link CardHeader} section. */
 interface CardHeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+/**
+ * Top section of a card, typically used to stack a title and description.
+ */
 const CardHeader = React.forwardRef<HTMLDivElement, CardHeaderProps>(
   ({ className, ...props }, ref) => (
     <div
@@ -31,8 +40,12 @@ const CardHeader = React.forwardRef<HTMLDivElement, CardHeaderProps>(
 );
 CardHeader.displayName = "CardHeader";
 
+/** Props for the {@link CardTitle} heading. */
 interface CardTitleProps extends React.HTMLAttributes<HTMLHeadingElement> {}
 
+/**
+ * Heading rendered inside {@link CardHeader}.
+ */
 const CardTitle = React.forwardRef<HTMLHeadingElement, CardTitleProps>(
   ({ className, ...props }, ref) => (
     <h3
@@ -44,8 +57,12 @@ const CardTitle = React.forwardRef<HTMLHeadingElement, CardTitleProps>(
 );
 CardTitle.displayName = "CardTitle";
 
+/** Props for the {@link CardDescription} body text. */
 interface CardDescriptionProps extends React.HTMLAttributes<HTMLParagraphElement> {}
 
+/**
+ * Subtle supporting text shown beneath a {@link CardTitle}.
+ */
 const CardDescription = React.forwardRef<HTMLParagraphElement, CardDescriptionProps>(
   ({ className, ...props }, ref) => (
     <p
@@ -57,8 +74,12 @@ const CardDescription = React.forwardRef<HTMLParagraphElement, CardDescriptionPr
 );
 CardDescription.displayName = "CardDescription";
 
+/** Props for the {@link CardContent} body region. */
 interface CardContentProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+/**
+ * Main content area of a card, rendered between header and footer.
+ */
 const CardContent = React.forwardRef<HTMLDivElement, CardContentProps>(
   ({ className, ...props }, ref) => (
     <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
@@ -66,8 +87,12 @@ const CardContent = React.forwardRef<HTMLDivElement, CardContentProps>(
 );
 CardContent.displayName = "CardContent";
 
+/** Props for the {@link CardFooter} region. */
 interface CardFooterProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+/**
+ * Bottom action area of a card, typically used for buttons.
+ */
 const CardFooter = React.forwardRef<HTMLDivElement, CardFooterProps>(
   ({ className, ...props }, ref) => (
     <div
